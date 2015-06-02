725c9729027dd36700dc7cf791e02fb9cfed9a40
kulili2008 <Kotori@KotorideRetina-MacBook-Pro.local>
2015-06-02 10:21:39 +0800

    Add AnimationUtil

---
+++ Classes/AnimationUtil.h
@@ -0,0 +1,17 @@
+#ifndef __AnimationUtil_H__
+#define __AnimationUtil_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class AnimationUtil
+{
+public:
+
+    static Animation* createWithSingleFrameName(const char* name, float delay, int iLoops);
+    
+
+    static Animation* createWithFrameNameAndNum(const char* name, int iNum, float delay, int iLoops);
+};
+
+#endif
