2a5a92dcb5898f7a780f9ac3283a155b635905f4
Yizhe <yuanyizhe@gmail.com>
2015-06-02 11:55:34 +0800

    Update Camera

--- Classes/BaseCamera.cpp
+++ Classes/BaseCamera.cpp
@@ -28,16 +28,22 @@ void BaseCamera::setTraceNode(BasePlayer* traceNode)
 
 void BaseCamera::update(float deltaTime)
 {
-	// auto cameraPosition = Director::getInstance()->getRunningScene()->getDefaultCamera()->getPosition();
-	// log("CAMERA X: %f Y: %f", cameraPosition.x, cameraPosition.y);
+	auto cameraPosition = Director::getInstance()->getRunningScene()->getDefaultCamera()->getPosition3D();
+	// Director::getInstance()->getRunningScene()->getDefaultCamera()->lookAt()
+	log("CAMERA X: %f Y: %f Z:%f", cameraPosition.x, cameraPosition.y,cameraPosition.z);
 	if (traceNode != nullptr)
 	{
-		auto positionDelta = this->getPosition() - Vec2(ConfigUtil::visibleWidth / 2, ConfigUtil::visibleHeight / 2) + traceNode->getPosition()*scaleCoefficient;
+		// auto positionDelta = this->getPosition() - Vec2(ConfigUtil::visibleWidth / 2, ConfigUtil::visibleHeight / 2) + traceNode->getPosition()*scaleCoefficient;
 		// log("P Delta X: %f Y: %f", positionDelta.x, positionDelta.y);
-		this->setPosition(this->getPosition() - positionDelta * traceNode->getTraceCoefficient() * deltaTime);
+		// this->setPosition(this->getPosition() - positionDelta * traceNode->getTraceCoefficient() * deltaTime);
 		// Use Default Camera
-		// auto positionDelta = traceNode->getPosition() + Vec2(ConfigUtil::visibleWidth / 2, ConfigUtil::visibleHeight / 2) - Director::getInstance()->getRunningScene()->getDefaultCamera()->getPosition();
-		// Director::getInstance()->getRunningScene()->getDefaultCamera()->setPosition(Director::getInstance()->getRunningScene()->getDefaultCamera()->getPosition() + positionDelta * traceNode->getTraceCoefficient() * deltaTime);
+		auto positionDelta = traceNode->getPosition() + Vec2(ConfigUtil::visibleWidth / 2, ConfigUtil::visibleHeight / 2) - Director::getInstance()->getRunningScene()->getDefaultCamera()->getPosition();
+		auto eye = Vec3(Director::getInstance()->getRunningScene()->getDefaultCamera()->getPosition3D() + Vec3(positionDelta.x * traceNode->getTraceCoefficient() * deltaTime, positionDelta.y * traceNode->getTraceCoefficient() * deltaTime, 0.0f));
+		Director::getInstance()->getRunningScene()->getDefaultCamera()->setPosition3D(eye);
+		Director::getInstance()->getRunningScene()->getDefaultCamera()->lookAt(eye);
+		
+		// Director::getInstance()->getRunningScene()->getDefaultCamera()->setPosition3D(Vec3(eyeX,eyeY,0.0f));
+		// Director::getInstance()->getRunningScene()->getDefaultCamera()->lookAt(Vec3(eyeX, eyeY, 0.0f));
 	}
 	// if (Controller::getKeyE() == true)
 	// {
